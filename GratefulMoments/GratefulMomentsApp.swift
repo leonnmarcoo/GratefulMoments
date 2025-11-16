a073d2b24dc27c48a0ea07737adfa482ad38e398
leonnmarcoo <develaleonmarco@gmail.com>
2025-11-16 14:04:05 +0800

    Initial Commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Leon Marco Devela on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
