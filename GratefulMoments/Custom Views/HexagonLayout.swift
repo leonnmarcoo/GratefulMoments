81bae44b2b911acb0f2536a51eb4fd897ca2af95
leonnmarcoo <develaleonmarco@gmail.com>
2025-11-18 09:27:05 +0800

    Create MomentHexagonView.swift

--- GratefulMoments/Custom Views/HexagonLayout.swift
+++ GratefulMoments/Custom Views/HexagonLayout.swift
@@ -5,7 +5,7 @@
 //  Created by Leon Marco Devela on 11/17/25.
 //
 
-import Foundation
+import SwiftUI
 
 enum HexagonLayout {
     case standard
@@ -17,6 +17,36 @@ enum HexagonLayout {
             return 200.0
         case .large:
             return 350.0
+        }
+    }
+    
+    var timestampBottomPadding: CGFloat {
+        0.08
+    }
+    
+    var textBottomPadding: CGFloat {
+        0.25
+    }
+    
+    var timestampHeight: CGFloat {
+        size * (textBottomPadding - timestampBottomPadding)
+    }
+    
+    var titleFont: Font {
+        switch self {
+        case .standard:
+            return .headline
+        case .large:
+            return .title.bold()
+        }
+    }
+    
+    var bodyFont: Font {
+        switch self {
+        case .standard:
+            return .caption2
+        case .large:
+            return .body
         }
     }
 }
--- GratefulMoments/Tabs/Moments/MomentsView.swift
+++ GratefulMoments/Tabs/Moments/MomentsView.swift
@@ -8,11 +8,16 @@
 import SwiftUI
 import SwiftData
 
+
 struct MomentsView: View {
     @State private var showCreateMoment = false
     @Query(sort: \Moment.timestamp)
     private var moments: [Moment]
-    
+
+
+    static let offsetAmount: CGFloat = 70.0
+
+
     var body: some View {
         NavigationStack {
             ScrollView {
@@ -40,27 +45,44 @@ struct MomentsView: View {
                     }
                 }
             }
+            .defaultScrollAnchor(.bottom, for: .initialOffset)
+            .defaultScrollAnchor(.bottom, for: .sizeChanges)
+            .defaultScrollAnchor(.top, for: .alignment)
             .navigationTitle("Grateful Moments")
         }
     }
-    
+
+
     private var pathItems: some View {
-        ForEach(moments) { moment in
+        ForEach(moments.enumerated(), id: \.0) { index, moment in
             NavigationLink {
                 MomentDetailView(moment: moment)
             } label: {
-                Text(moment.title)
+                if moment == moments.last {
+                    MomentHexagonView(moment: moment, layout: .large)
+                } else {
+                    MomentHexagonView(moment: moment)
+                        .offset(x: sin(Double(index) * .pi / 2) * Self.offsetAmount)
+                }
+            }
+            .scrollTransition { content, phase in
+                content
+                    .opacity(phase.isIdentity ? 1 : 0)
+                    .scaleEffect(phase.isIdentity ? 1 : 0.8)
             }
         }
     }
 }
+
 
 #Preview {
     MomentsView()
         .sampleDataContainer()
 }
 
+
 #Preview("No moments") {
     MomentsView()
         .modelContainer(for: [Moment.self])
+        .environment(DataContainer())
 }
